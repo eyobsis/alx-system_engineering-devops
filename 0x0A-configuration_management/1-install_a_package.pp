034a287f39a00bf577851b9c8fee21154e3aed00
eyobsis <ehtube2020@gmail.com>
2023-11-29 15:38:37 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install a espesific version of flask 2.1.0
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+# Puppet manifest to install Flask using pip3
+
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
