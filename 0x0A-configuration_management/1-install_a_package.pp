5dfdf4ad6f3623a05cdccd75fd3eaf8e443ce6ab
eyobsis <ehtube2020@gmail.com>
2023-11-29 15:14:15 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,6 @@
 #!/usr/bin/pup
-# install a esesific version of flask 2.1.0
-
-package { 'python3-pip':
-  ensure => installed,
+# install a espesific version of flask 2.1.0
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
-}
-
-file { '/usr/local/bin/flask':
-  ensure => link,
-  target => '/usr/local/bin/flask',
-  require => Exec['install_flask'],
-}
-
