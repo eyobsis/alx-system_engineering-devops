9c6f13858971261af5725a91f4e4482c9757aef9
eyobsis <ehtube2020@gmail.com>
2023-11-29 17:58:52 -0800

    nginx pup

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,43 @@
-# Puppet manifest to install Nginx web server and configure it
-exec { 'server configuration':
-  provider => shell,
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && echo "Hello World!" > /var/www/html/index.html && sudo sed -i "/server_name _;/a location /redirect_me {\n\treturn 301 https://google.com; listen 80;\n\t}\n" /etc/nginx/sites-available/default && sudo service nginx restart'
+# 7-puppet_install_nginx_web_server.pp
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
+
+# Set up the default HTML page
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# Configure Nginx to listen on port 80 and set the root directory
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 https://www.example.com/new_page;
+      }
+    }
+  ",
+}
+
+# Create a symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+}
+
+# Restart Nginx service to apply changes
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-enabled/default'],
+}
+
