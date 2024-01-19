7fef8e1eb369e13adfea21454c9f8f057a8f074e
eyobsis <ehtube2020@gmail.com>
2024-01-18 17:38:17 -0800

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts listed
+prints the titles of the first 10 hot posts listed
 for a given subreddit.
 """
 import requests
@@ -8,12 +8,12 @@ import requests
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
+    prints the titles of the first 10 hot posts
     listed for a given subreddit.
     """
     url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
         subreddit)
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
+    headers = {'User-Agent': 'Python/1.0(alx School 0x16)'}
     response = requests.get(url, headers=headers)
     try:
         top_ten = response.json()['data']['children']
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns a list containing the titles of all hot
+returns a list containing the titles of all hot
 articles for a given subreddit.
 """
 import requests
@@ -8,7 +8,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], count=0, after=''):
     """
-    Queries the Reddit API and returns a list containing the titles of all hot
+     returns a list containing the titles of all hot
     articles for a given subreddit.
     """
     base = 'https://www.reddit.com/'
