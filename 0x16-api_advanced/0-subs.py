c82c3d3095660a35ad31d9a042fa092b512a233a
eyobsis <ehtube2020@gmail.com>
2024-01-18 18:04:33 -0800

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
 """
-returns the number of total subscribers for a given
-subreddit.
+Queries Reddit API and returns the number of subscribers for a given subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    returns number of total subscribers for a
-    given subreddit.
+    Queries Reddit API and returns number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: number of subscribers, or 0 if the subreddit is invalids.
     """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(School 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    url = f'http://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            return response.json().get('data').get('subscribers', 0)
+        else:
+            return 0
+
+    except requests.exceptions.RequestException as error:
+        print(f"Error: {error}")
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit_name = sys.argv[1]
+        subscribers_count = number_of_subscribers(subreddit_name)
+        print(subscribers_count)
